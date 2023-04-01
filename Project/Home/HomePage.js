cc253b2a4914bcedd3d02390bb55d68b14ff0549
Gia Thinh Hoang <jiathinhj@jiathinhjs-mac.local>
2023-04-02 00:04:02 +0700

    add models, ultils, and adjust to use BS4 only

--- Project/Home/HomePage.js
+++ Project/Home/HomePage.js
@@ -24,13 +24,13 @@ function displayProduct(listProduct) {
   var result = "";
   listProduct.forEach(element => {
     result +=
-      `<div class="col-3 col-sm-3 col-lg-3">
+      `<div class="col-3 col-sm-3 ">
     <div class="row">
-      <div class="col-12 col-sm-12 col-lg-12">`
-      + `<img src="` + element.getImageSoure + `" alt="" style="height: 300px;">`
-      + `<h3  style="font-weight:bold;">` + element.getName + `</h3>`
-      + `<h4>Hãng sản xuất: ` + element.getManufacturer + `</h4>`
-      + ` <ul class="rating" style="list-style: none; display: flex; font-size: 20px; padding-inline-start: 0px">
+      <div class="col-12 col-sm-12 ">`
+      + `<div class="row"> <img src="` + element.getImageSoure + `" alt="" style="height: 300px;"> </div>`
+      + `<div class="row"> <h3  style="font-weight:bold;">` + element.getName + `</h3> </div>`
+      + `<div class="row"> <h4>Hãng sản xuất: ` + element.getManufacturer + `</h4> </div>`
+      + ` <div class="row"> <ul class="rating" style="list-style: none; display: flex; font-size: 20px; padding-inline-start: 0px">
           <li>
             <p class="fa fa-star selected" style="color: orange"></p>
           </li>
@@ -46,12 +46,12 @@ function displayProduct(listProduct) {
           <li>
             <p class="fa fa-star selected" ></p>
           </li>
-        </ul>
+        </ul> </div>
         <div class="row">
-          <div class="col-sm-4 col-md-4 col-lg-4">
+          <div class="col-7 col-sm-7  ">
             <h4>`+ element.getPrice + `</h4>
           </div>
-          <div class="ccol-sm-6 col-md-6 col-lg-6">
+          <div class="col-4 col-sm-4  ">
             <button type="button" class="btn btn-default" style="border: 0px">
               <p class="fa fa-shopping-cart" style="color: red; font-size: 35px"></p>
             </button>
